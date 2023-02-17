393edbf2bec6c2640ee59469b493e6ca17c4ee75
wisdom <wisdomngwuueche@mail.com>
2023-02-16 20:47:32 -0800

    another

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int a;
